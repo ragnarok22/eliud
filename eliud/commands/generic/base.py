7952896b7d282d499d46ce68e14590fe284851dd
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-09 09:28:55 -0500

    feat: add basic structure of a Command

--- eliud/commands/generic/base.py
+++ eliud/commands/generic/base.py
@@ -13,7 +13,24 @@ def _validate_command(command: str):
         raise CommandException(f"Command {command} is not a valid command")
 
 
-class Command:
-    def __init__(self, command: str):
-        _validate_command(command)
-        self.command = command
+class BaseCommand:
+    def __init__(self, **kwargs):
+        """
+        Constructor. Called in the CommandConf; can contain helpful extra
+        keyword arguments, and other things.
+        """
+        # Go through keyword arguments, and either save their values to our
+        # instance, or raise an error.
+        for key, value in kwargs.items():
+            if key == "command":
+                _validate_command(value)
+            setattr(self, key, value)
+
+
+class Command(BaseCommand):
+    command: str = None
+    markup = None
+    markup_kwargs: dict = None
+
+    def render_markup(self, **kwargs):
+        return self.markup(**kwargs)
