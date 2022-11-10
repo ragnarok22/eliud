6fe903e8a0b511beeddc3588b1a5b4e3531e6c39
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-10 10:36:14 -0500

    perf: add runtests

--- tests/README.md
+++ tests/README.md
@@ -2,4 +2,5 @@ To run the test suite, first, create and activate a virtual environment.
 Then install some requirements and run the tests:
 
     $ poetry install
-    $ poetry run python tests/runtests.py
+    $ cd tests
+    $ poetry run python runtests.py
--- tests/runtests.py
+++ tests/runtests.py
@@ -1,3 +1,9 @@
+import glob
+import inspect
+import os
+import sys
+import unittest
+
 try:
     import eliud  # noqa: F401
 except ImportError as e:
@@ -5,5 +11,31 @@ except ImportError as e:
         "Eliud module not found, reference tests/README.md for instructions."
     ) from e
 
+RUNTESTS_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
 if __name__ == "__main__":
-    pass
+
+    def get_name_from_path(path: str) -> str:
+        return path.replace(os.path.sep, ".").removesuffix(".py")
+
+    tests_list = glob.glob("test_*")
+    print(tests_list)
+    suite = []
+    for test_target in tests_list:
+        if os.path.isdir(test_target):
+            for test_file in glob.glob(f"{test_target}/test_*"):
+                suite.append(
+                    unittest.defaultTestLoader.loadTestsFromName(
+                        get_name_from_path(test_file)
+                    )
+                )
+        else:
+            suite.append(
+                unittest.defaultTestLoader.loadTestsFromName(
+                    get_name_from_path(test_target)
+                )
+            )
+
+    main_suite = unittest.TestSuite(suite)
+    unittest.TextTestRunner(verbosity=1).run(main_suite)
