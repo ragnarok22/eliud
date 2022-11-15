8aae41189039b35e272e792bbeb4a9961fff4351
Reinier Hernández <sasuke.reinier@gmail.com>
2022-11-15 13:12:06 -0500

    feat: add telegram module

---
+++ eliud/telegram/__init__.py
@@ -0,0 +1,81 @@
+import logging
+
+import telegram
+from telegram.error import TelegramError
+from telegram.ext import CommandHandler, Updater
+
+from eliud.commands import Command
+from eliud.conf import settings
+from eliud.markups.base import BaseMarkup
+from eliud.telegram import utils
+
+logger = logging.getLogger("eliud.telegram")
+
+
+class Bot:
+    """
+    Constructs a bot and handles all the interactions
+    """
+
+    def __init__(self):
+        token = settings.TELEGRAM_TOKEN
+        self.bot = telegram.Bot(token=token)
+        self.updater = Updater(
+            token=token, use_context=True, workers=settings.TELEGRAM_WORKERS
+        )
+        self.dispatcher = self.updater.dispatcher
+        self.commands_descriptions = []
+
+    def add_callback(self, handler, pattern, run_async=False):
+        """
+        Add Callback action
+        :param handler:
+        :param pattern:
+        :param run_async:
+        :return:
+        """
+        self.dispatcher.add_handler(handler, pattern=pattern, run_async=run_async)
+
+    def add_command(self, command: Command):
+        """
+        Add command action
+        :param command:
+        :return:
+        """
+        self.dispatcher.add_handler(
+            CommandHandler(
+                command.command, command.get_handle, run_async=command.is_async
+            )
+        )
+
+        if command.description:
+            self.commands_descriptions.append((command.command, command.description))
+
+    def __set_descriptions(self):
+        """
+        Set commands descriptions in Telegram
+
+        :return:
+        """
+        self.bot.set_my_commands(self.commands_descriptions)
+
+    def send_message(self, chat_id: str, markup: BaseMarkup):
+        try:
+            self.bot.send_message(chat_id=chat_id, text=markup.get_text())
+        except TelegramError as e:
+            logger.error(e)
+
+    def start(self):
+        """
+        Start the bot in development mode
+
+        """
+        self.__set_descriptions()
+        self.updater.start_polling()
+        self.updater.idle()
+        logger.info(f"Running at https://t.me/{self.bot.username}")
+
+
+bot = Bot()
+
+__all__ = ["bot"]
---
+++ eliud/telegram/utils.py
@@ -0,0 +1,17 @@
+import logging
+
+from telegram.error import TelegramError
+
+from ..markups.base import BaseMarkup
+from . import bot
+
+logger = logging.getLogger("eliud.telegram")
+
+
+def send_message(chat_id: str, markup: BaseMarkup):
+    try:
+        bot.send_message(chat_id=chat_id, text=markup)
+        return True
+    except TelegramError as e:
+        logger.error(e)
+        return False
